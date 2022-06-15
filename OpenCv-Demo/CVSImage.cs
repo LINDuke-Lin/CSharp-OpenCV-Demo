3a64a93994225abb3751d552b2285c021813bf2c
chicken <hibary.taiwan@gmail.com>
2022-06-15 20:33:40 +0800

    init

---
+++ OpenCv-Demo/CVSImage.cs
@@ -0,0 +1,22 @@
+﻿using OpenCvSharp;
+
+namespace OpenCv_Demo
+{
+    public class CVSImage
+    {
+        private Mat SrcImg { get; }
+
+        public CVSImage(string imgPath, int readMode = 1)
+        {
+            SrcImg = Cv2.ImRead(imgPath, (ImreadModes)readMode);
+        }
+
+        public void ShowImage(string winName, int windowType = 1, int showTime = 0)
+        {
+            WindowMode mode = (WindowMode)windowType;
+            Cv2.NamedWindow(winName, mode);
+            Cv2.ImShow(winName, SrcImg);
+            Cv2.WaitKey(showTime);
+        }
+    }
+}
---
+++ OpenCv-Demo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OpenCv_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            CVSImage cVS = new CVSImage(@"C:\Duke\C#dev\Rust.png");
+
+            cVS.ShowImage("OOP");
+        }
+    }
+}
